2b22ea1309a424bcdfa80e8e1d3bc32601517887
JAESEUNG JEONG <jaeseungjeong@JAESEUNGui-MacBookPro.local>
2022-02-17 16:28:03 +0900

    edit the file

--- Chapter3_Greedy/Law of large numbers/Law_of_large_number.py
+++ Chapter3_Greedy/Law of large numbers/Law_of_large_number.py
@@ -1,6 +1,21 @@
 #law of large number
 
-
 n, m ,k = map(int, input().split())
 
-print(n)
+if m >= k:
+    n_list = list(map(int, input().split()[ :n]))
+
+else:
+    print("please enter M lager than K")
+    exit()
+    
+n_list.sort(reverse = True)
+first = n_list[0]
+second = n_list[1]
+
+count_first = int(m/(k+1))*k + m%(k+1)
+count_second = m - count_first
+
+largest_number = first * count_first + second * count_second
+
+print(largest_number)
