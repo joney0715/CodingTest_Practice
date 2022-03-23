1502bcc15ca4563b667ef6386772d6b56724b94a
JAESEUNG JEONG <jaeseungjeong@JAESEUNGui-MacBookPro.local>
2022-03-23 14:05:25 +0900

    add file

---
+++ parenthesize/parenthesize.py
@@ -0,0 +1,13 @@
+
+#def parenthesize(data):
+#    answer = []
+#
+#    data = [i for i in data]
+#    answer = data
+#    return answer
+
+#data1 = "[{()}]"
+#data2 = "[}"
+#data3 = "{[]}"
+
+print(1)
