acb3bad4be0d10118310a2d064668e8a82c87ef4
joney0715 <joney0715@gmail.com>
2022-07-21 20:34:04 +0900

    edit file

--- BOJ/2304.py
+++ BOJ/2304.py
@@ -1,28 +1,39 @@
-'''
-N = int(input())
-LHs = [0] * 1001
-for i in range(N):
-    l, h = map(int, input().split())
-    LHs[l] = h
-'''
 
-N = 7
-LHs = [0,0,4,0,6,3,0,0,10,0,0,4,0,6,0,8]
+#N = int(input())
+n = 1001
+LH = [0] * n
+# for i in range(N):
+#     l, h = map(int, input().split())
+#     LH[l] = h
+LH[2] = 4
+LH[11] = 4
+LH[15] = 8
+LH[4] = 6
+LH[5] = 3
+LH[8] = 10
+LH[13] = 6
 
-def solution(LHs):
-    H = 0
-    s = 0
-    L_past = 0
-    for L in range(len(LHs)):
-        if LHs[L] != 0:
-            s += H * (L - L_past)
-            L_past = L
-            H = LHs[L]
-        else:
-            continue
-    return s
+H_max = max(LH)
+id_max = 0
+for idx in range(n):
+    if LH[idx] == H_max:
+        id_max = idx
+        break
+   
+answer = H_max
+h_r = 0
+for i in range(n-1, id_max, -1):
+    if LH[i] > h_r:
+        h_r = LH[i]
+    answer += h_r
 
-print(solution(LHs))
+h_l = 0
+for j in range(0, id_max):
+    if LH[j] > h_l:
+        h_l = LH[j]
+    answer += h_l
+
+print(answer)
 
 
 
